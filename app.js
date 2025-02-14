754090bf533153ff187b61048b92afd83bfea9c8
rajat sharma <rajatsharma0716@gmail.com>
2025-02-14 11:52:44 +0530

    new file changes

---
+++ app.js
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2> Javascript Main Page</h2>
+
+<p id="main"> Javascript changes</p>
+<button type-"button" onclick-'document.getElementById("main").innerHTML = "Hey user Welcome!"'> Submit </button>
+</body>
+</html>
